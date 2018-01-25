906abc32e77ae8bcd1920c5584a40e699a16261f
Thomas Smallridge <thomas@studentedge.com.au>
2018-01-25 12:14:48 +0800

    Working with terrain types and colour, oooooo

--- Assets/_Scripts/MapDisplay.cs
+++ Assets/_Scripts/MapDisplay.cs
@@ -6,28 +6,11 @@ namespace Assets._Scripts
     {
         public Renderer TextureRenderer;
 
-        public void DrawNoiseMap(float[,] noiseMap)
+        public void DrawTexture(Texture2D texture)
         {
-            int width = noiseMap.GetLength(0);
-            int height = noiseMap.GetLength(1);
-
-            var texture = new Texture2D(width, height);
-            
-            Color[] colourMap = new Color[width * height];
-            for (int y = 0; y < height; y++)
-            {
-                for (int x = 0; x < width; x++)
-                {
-                    colourMap[y * width + x] = Color.Lerp(Color.black, Color.white, noiseMap[x, y]);
-                }
-            }
-
-            texture.SetPixels(colourMap);
-            texture.Apply();
-
             TextureRenderer.sharedMaterial.mainTexture = texture;
             //transform plane to our texture size
-            TextureRenderer.transform.localScale = new Vector3(width, 1, height);
+            TextureRenderer.transform.localScale = new Vector3(texture.width, 1, texture.height);
         }
     }
 }
--- Assets/_Scripts/MapGenerator.cs
+++ Assets/_Scripts/MapGenerator.cs
@@ -6,23 +6,98 @@ namespace Assets._Scripts
     {
         //https://www.youtube.com/watch?v=WP-Bm65Q-1Y
 
+        public enum DrawingMode
+        {
+            NoiseMap,
+            ColourMap
+        }
+
+        public DrawingMode DrawMode;
+
         public int Width;
         public int Height;
         public float NoiseScale;
+        public int Octaves;
+        [Range(0,1)]
+        public float Persistance;
+        public float Lacunarity;
+        public Vector2 Offset;
+        public int Seed;
+
+        public bool UseRandomSeed;
         public bool AutoUpdate;
 
-        void Update()
-        {
-            if (Input.GetButtonDown("Fire1"))
-            {
-                GenerateMap();
-            }
-        }
+        public TerrainTypes[] Regions;
+
         public void GenerateMap ()
         {
-            float[,] noiseMap = Noise.GenerateNoiseMap(Width, Height, NoiseScale);
+            if (UseRandomSeed)
+            {
+                Seed = Time.time.GetHashCode();
+            }
+
+            float[,] noiseMap = Noise.GenerateNoiseMap(Width, Height, Seed, NoiseScale, Octaves, Persistance, Lacunarity, Offset);
+
+            Color[] colourMap = new Color[Width*Height];
+
+            for (int y = 0; y < Height; y++)
+            {
+                for (int x = 0; x < Width; x++)
+                {
+                    float currentHeight = noiseMap[x, y];
+                    for (int i = 0; i < Regions.Length; i++) //loop through regions, assigning the correct type based on height!
+                    {
+                        if (currentHeight <= Regions[i].Height)
+                        {
+                            colourMap[y * Width + x] = Regions[i].Colour;
+                            break;
+                        }
+                    }
+                }
+            }
+
             MapDisplay display = FindObjectOfType<MapDisplay>();
-            display.DrawNoiseMap(noiseMap);
+            if (DrawMode == DrawingMode.NoiseMap)
+            {
+                display.DrawTexture(TextureGenerator.TextureFromHeightMap(noiseMap));
+            }
+            else if (DrawMode == DrawingMode.ColourMap)
+            {
+                display.DrawTexture(TextureGenerator.TextureFromColourMap(colourMap, Width, Height));
+            }
+            
         }
+
+        void OnValidate()
+        {
+            if (Width < 1)
+            {
+                Width = 1;
+            }
+
+            if (Height < 1)
+            {
+                Height = 1;
+            }
+
+            if (Lacunarity < 1)
+            {
+                Lacunarity = 1;
+            }
+
+            if (Octaves < 0)
+            {
+                Octaves = 0;
+            }
+
+        }
+    }
+
+    [System.Serializable]
+    public struct TerrainTypes
+    {
+        public string Name;
+        public float Height;
+        public Color Colour;
     }
 }
