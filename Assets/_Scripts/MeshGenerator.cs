98b5534f0c32773de9c43ce491d305daae41a59b
Thomas Smallridge <thomas@studentedge.com.au>
2018-01-25 14:39:14 +0800

    Added stub for meshgenerator

---
+++ Assets/_Scripts/MeshGenerator.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace Assets._Scripts
+{
+
+    //Following https://www.youtube.com/watch?v=4RpVBYW1r5M
+    //Could use the existing map generation with a voxel world?
+    public static class MeshGenerator {
+
+        public static void GenerateTerrainMesh(float[,] heightMap)
+        {
+            int width = heightMap.GetLength(0);
+            int height = heightMap.GetLength(1);
+
+            for (int y = 0; y < height; y++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    
+                }
+            }
+        }
+    }
+}
